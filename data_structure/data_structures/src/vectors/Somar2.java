a487c8451b04f68efe3c082a941455f793a4bc3a
Tarcísio Lima Ferreira <tarcisiolf0@gmail.com>
2024-03-08 18:18:34 -0300

    Adicionado pacote no código

--- data_structure/data_structures/src/vectors/Somar2.java
+++ data_structure/data_structures/src/vectors/Somar2.java
@@ -1,19 +1,21 @@
+package vectors;
+
 import javax.swing.JOptionPane;
 
 public class Somar2 {
     public static void main(String[] args) {
-        
-    int matSoma [][], a, b, soma = 0;
-    matSoma = new int [10][12];
 
-    for (a = 0; a <= 9; a++) {
-        for (b = 0; b <= 11; b++) {
-            matSoma[a][b] = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro"));
-            soma = soma + matSoma[a][b];
+        int matSoma[][], a, b, soma = 0;
+        matSoma = new int[10][12];
+
+        for (a = 0; a <= 9; a++) {
+            for (b = 0; b <= 11; b++) {
+                matSoma[a][b] = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro"));
+                soma = soma + matSoma[a][b];
+            }
         }
-    }
 
-    System.out.println("A soma dos 120 valores digitados é: " + soma);
-    System.exit(0);
-  }
+        System.out.println("A soma dos 120 valores digitados é: " + soma);
+        System.exit(0);
+    }
 }
